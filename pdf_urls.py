ebceaa13c2c05bec566830bd9fe5203b10a9c681
Alex <alexanderdrichardson@gmail.com>
2024-10-13 02:49:01 -0700

    blank out certs

--- pdf_urls.py
+++ pdf_urls.py
@@ -1,5 +1,5 @@
 #Note the "fl" after the opensesame domain. PDF URLS will need to have this inserted for the script to run correctly. 
 pdf_urls = [
-    "https://csr.opensesame.com/fl/completion_certificate/ce746c94-5ec6-a414-e5e4-aa4bf31d886e/bb7e063c-b87a-4f16-ac64-73efbf87dbe5/914e2b35-2e34-4e44-2915-5bccd09bdbb3",
-    "https://csr.opensesame.com/fl/completion_certificate/ce746c94-5ec6-a414-e5e4-aa4bf31d886e/bb7e063c-b87a-4f16-ac64-73efbf87dbe5/a1e009a4-73c3-43b4-b92f-4c4edf2eb22c"
+    "https://csr.opensesame.com/fl/",
+    "https://csr.opensesame.com/fl/"
 ]
